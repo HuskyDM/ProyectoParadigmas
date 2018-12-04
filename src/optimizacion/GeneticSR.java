c4139a4c8825dcbd9effab2dcb606a99d5fc74f3
eduar <eduar@DESKTOP-SQ8QRB1>
2018-12-04 12:54:44 -0600

    Correcciones en geneticSR

--- src/optimizacion/GeneticSR.java
+++ src/optimizacion/GeneticSR.java
@@ -12,7 +12,6 @@ import java.util.Random;
  * @author Jay
  */
 public class GeneticSR {
-    int i;
     Random r = new Random();
     
     public Population mutatePopulationSR(Population population) {
@@ -20,27 +19,24 @@ public class GeneticSR {
                             //determining the chance that individuals either outside the parameter boundaries
                             //or violating the constraints are compared during the selection. The default is '0.475'.
 
-        // Initialize new population
-	Population newPopulation = new Population(population.size());
-        
+        Population newPopulation = new Population(population.size());
         boolean wasSwapped = false;
 
-        // Loop over current population by fitness
         for (int populationIndex = 0; populationIndex < population.size(); populationIndex++) {
-            // within bounds or random chance
-            
             //Se agrega al gen el random
-            
             //double noise = r.nextGaussian() * Math.sqrt(variance) + mean;
             double random = r.nextGaussian() * 0.1 + 0;
+            
             //individual.setInfection-rate(individual.getInfection-rate + random);
             //individual.setfluorescence(individual.getfluorescence + random);
             
+            // within bounds or random chance
             if ((phi(populationIndex) == 0 && phi(populationIndex + 1) == 0) || UniformRandom.uniform(0, 1) < pf) {
                 //Comparación de Value de geneticSR
                 
                 //if (Value(j) > Value(j + 1))
-                if (population.getFittest(populationIndex).getFitness() > population.getFittest(populationIndex+1).getFitness()) {
+                if (population.getIndividual(populationIndex).getFitness()
+                        > population.getIndividual(populationIndex + 1).getFitness()) {
                     swap(populationIndex, populationIndex + 1);
                     wasSwapped = true;
                 }
@@ -55,23 +51,19 @@ public class GeneticSR {
             //newPopulation.setIndividual(populationIndex, individual);
         }
         
-        //if (wasSwapped == false) break;
+        if (wasSwapped == false) {
+            return population;
+        }
         
-        // Return mutated population
         return newPopulation;
     }
     
-    private int phi(int valor) {
-        
-        
-        
+    private int phi(double valor) {
         
         return 0;
     }
     
     private void swap(int valor1, int valor2) {
-    
-    
-    
+        
     }
 }
